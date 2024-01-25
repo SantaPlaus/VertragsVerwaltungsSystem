a2659a1518ec02c0f9112ae2c8d9a1ae1570930d
I050698 <zachariaspaul.achenbach@axa.de>
2024-01-25 11:35:18 +0100

    neue Anweisungen aufgeschrieben

---
+++ src/main/java/org/vertragsverwaltung/Model/KundenRequest.java
@@ -0,0 +1,15 @@
+package org.vertragsverwaltung.Model;
+
+public class KundenRequest {
+    //TODO JSONObject muss hier rein
+    // Jedes einzelne (auch FileRepository)
+
+    /**
+     * PLAN
+     * - nicht immer werden JSON Dateien eingelesen, also muss man das Programm dementsprechend ändern können
+     * - Inputwerte und -format können sich auf Kundenanfrage ändern
+     * - man soll NUR in KundenRequest die Werte ändern müssen
+     * - FileReader etc. wird in KundenRequest erstellt (Man kann diesen in FileRepository aufrufen(?))
+     *
+     */
+}
